4b096e330e424766663f04f19ebcf6fc7a42c2a6
Tree Star Systems <info@treestarsystems.com>
2021-12-05 03:00:19 -0500

    Updating install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 echo -e "This must be ran from the app's root directory. Ex: /opt/appname\n"
+echo -e "Making system level changes\n"
+echo -e "Enabling One Wire Temperature Sensor\n"
+sudo echo -e "\n# Enable use of One Wire Temperature Sensors\ndtoverlay=w1-gpio\n" >> /boot/config.txt
+sudo modprobe w1-gpio
+sudo modprobe w1-therm
 echo -e "Installing App Specific Dependencies...\n"
 sudo /usr/bin/npm i johnny-five --save
+
+#sudo reboot
