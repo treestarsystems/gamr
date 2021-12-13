98e64fac5c6a4119bc86b89f73319a81005c14eb
Young Grizzly <info@treestarsystems.com>
2021-12-13 02:23:54 -0500

    web ui initial commit

---
+++ server/view/public/js/tsscuts.js
@@ -0,0 +1,507 @@
+var pageSpecificTargetDiv;
+var tsscutsConfig = {
+ "projectName":"Tree Star Systems: CUTS",
+ "tsscutsSiteLink":"https://treestarsystems.com",
+ "tsscutsGithubLink":"https://github.com/treestarsystems",
+ "tsscutsYoutubeLink":"https://www.youtube.com/c/YoungGrizzly",
+ "tsscutsUpworkLink":"https://www.upwork.com/o/profiles/users/~0137ac5fc8e430492d/",
+ "tsscutsDocumentationLink":"https://treestarsystems.atlassian.net/wiki/spaces/TC/overview",
+ "tsscutsResidence":"Waycross, GA",
+ "tsscutsMap":`
+  <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src=
+   "https://maps.google.com/maps?q=Waycross%20Georgia%2C%20USA&amp;amp;t=m&amp;amp;z=16&amp;amp;output=embed&amp;amp;iwloc=near&amp;output=embed"
+  ></iframe>
+ `,
+ "tsscutsEmail":"",
+ "tsscutsPhone":"",
+ "tsscutsAboutMe":`Well rounded Network and Systems Administrator with experience in LAN/WAN/WLAN data and
+telecommunications, security practices and technologies, as well as troubleshooting
+approaches. I am a heavy user of open source alternatives to supplement proprietary/closed
+source environments to ensure uptime, security, and overall health while achieving a lower Total
+Cost of Ownership (TCO). My goal is to never work too hard doing a repetitive task but work
+hard to automate a task. I write code to make my job easier, streamline my department’s
+process, and make my colleagues' lives better by allowing them to work on tasks that really
+matter to them. My ultimate goal is to propel and accelerate the growth of my employer.`,
+ "tsscutsCompetancies":[
+  {
+   "logo": "fab fa-node competency-logo",
+   "imageAltText":"node js logo",
+   "text":"I love to code backend functionality. Ingesting, normalizing, and storing data is fun to me. I crave large data sets and work to find ways to process information as fast and reliably as possible.",
+   "competencyName":"Node.js",
+   "competencyFontAwesomeClassName":"fab fa-node-js",
+  },
+  {
+   "logo": "fab fa-js-square competency-logo",
+   "imageAltText":"vanilla js logo",
+   "text":"I work to make it user friendly, viusally pleasing, and highly functional. I have learned the hard way that if the front end is not working well for a user base the backend means absolutely nothing. ",
+   "competencyName":"vanilla JS",
+   "competencyFontAwesomeClassName":"fab fa-js",
+  },
+  {
+   "logo": "fab fa-html5 competency-logo",
+   "imageAltText":"HTML5 logo",
+   "text":"There is nothing like a well placed anchor tag or class that allows you to hydrate your page with dynamic content. HTML5 with a templating engine or framework opens up a ton of possibilities that will bring life to your page.",
+   "competencyName":"HTML5",
+   "competencyFontAwesomeClassName":"fab fa-html5",
+  },
+  {
+   "logo": "fab fa-css3-alt competency-logo",
+   "imageAltText":"CSS logo",
+   "text":"I have a love hate relationship with CSS. I love it when someone else does it for me, but hate it when I have to make it work. With the use of CSS Variables I can do all kinds of fun stuff. I mean how else do you think I make the colors change every time you reload this page? <i style='font-style:normal;'>&#129300;&#129299;</i>",
+   "competencyName":"CSS3",
+   "competencyFontAwesomeClassName":"fab fa-css3",
+  },
+ ],
+ "tsscutsPricingState":"off",
+ "tsscutsPricing":[
+  {
+   "pricingLevel":"Basic",
+   "pricingRate":"35",
+   "pricingList":[
+    "Item 1",
+    "Item 2",
+    "",
+    "",
+   ],
+  },
+  {
+   "pricingLevel":"Intermediate",
+   "pricingRate":"55",
+   "pricingList":[
+    "Includes Basic",
+    "",
+    "",
+    "",
+   ],
+  },
+  {
+   "pricingLevel":"Advance",
+   "pricingRate":"75",
+   "pricingList":[
+    "Includes Intermediate",
+    "",
+    "",
+    "",
+   ],
+  },
+ ],
+ "tsscutsEducation":[
+  {
+   "range":"2004-2006",
+   "gpa":"GPA 2.8",
+   "schoolName":"Missouri Valley College",
+   "fieldOfStudy":"Computer Science",
+   "description":"",
+  },
+  {
+   "range":"2006-2008",
+   "gpa":"GPA 2.8",
+   "schoolName":"Broward College",
+   "fieldOfStudy":"Associates of the Arts",
+   "description":"",
+  },
+ ],
+ "tsscutsExperience":[
+  {
+   "range":"Dec 2019-Present",
+   "companyName":"Hamilton Home Loans",
+   "title":"Tier 3 Support/Full Stack Developer",
+   "description":"",
+  },
+  {
+   "range":"2009-2019",
+   "companyName":"MJ Network Solutions",
+   "title":"Lead Technician/Senior Consultant",
+   "description":"",
+  },
+  {
+   "range":"Aug 2011–2020",
+   "companyName":"365 Datacenters",
+   "title":"Network Operations (NetOps)",
+   "description":"",
+  },
+  {
+   "range":"2008–2010",
+   "companyName":"DeRose Technologies Inc.",
+   "title":"Technician/Consultant",
+   "description":"",
+  },
+  {
+   "range":"2007-2008",
+   "companyName":"Reliant Medical",
+   "title":"Systems and Network Administrator",
+   "description":"",
+  },
+ ],
+ "tsscutsSkills":[
+  {
+   "skillTitle":"Coding",
+   "profeciencies":[
+    {
+     "name":"API Design",
+     "percentage":"75",
+    },
+    {
+     "name":"UI/UX Design",
+     "percentage":"50",
+    }
+   ]
+  },
+  {
+   "skillTitle":"Coding2",
+   "profeciencies":[
+    {
+     "name":"API Design",
+     "percentage":"75",
+    },
+    {
+     "name":"UI/UX Design",
+     "percentage":"50",
+    }
+   ]
+  }
+ ],
+ "tsscutsKnowledges": [
+  "Tai Lopez",
+  "NAH-LEDGE",
+  "stuf",
+  "the man",
+ ],
+ "tsscutsCertificates":[
+  {
+   "title":"CCNA (200-125)",
+   "vendor":"Cisco",
+   "id":"CSCO11806465",
+   "logo":"cisco.png",
+   "date":"Dec. 2019"
+  },
+  {
+   "title":"CompTIA A+",
+   "vendor":"CompTIA",
+   "id":"5FBBJY92EHQES5GF",
+   "logo":"comptia.png",
+   "date":""
+  },
+  {
+   "title":"CompTIA Network+",
+   "vendor":"CompTIA",
+   "id":"C07ZG2JJYKR1SST3",
+   "logo":"comptia.png",
+   "date":""
+  },
+  {
+   "title":"CompTIA Linux+",
+   "vendor":"CompTIA",
+   "id":"JK4DV87L63FESTV7",
+   "logo":"comptia.png",
+   "date":""
+  },
+  {
+   "title":"IPv6 Certified: Sage Level",
+   "vendor":"Hurricane Electric",
+   "id":"(Certificate Available)",
+   "logo":"hurricane-electric.png",
+   "date":""
+  },
+ ],
+ "tsscuts":"",
+}
+
+function fillInterface (obj) {
+ document.title = `${obj.projectName}`;
+
+ let ageDiv = document.getElementById('age');
+ let competenciesDiv = document.getElementById('competencies');
+ let pricingDivState = document.getElementById('tsscutsPricingState');
+ let knowledgesDiv = document.getElementById('tsscutsKnowledges');
+ let skillsDiv = document.getElementById('tsscutsSkills');
+ let certificatesDiv = document.getElementById('tsscutsCertificates');
+ let educationDiv = document.getElementById('tsscutsEducation');
+ let experienceDiv = document.getElementById('tsscutsExperience');
+
+ for (let key in obj) {
+  let element = document.querySelectorAll(`.${key}`);
+  element.forEach((e) => {
+   if (key.includes('Link')) {
+    e.href = obj[key];
+   } else {
+    e.innerHTML = obj[key].replace(/[\n\r]/g, ' ');
+   }
+  });
+ }
+ obj.tsscutsCompetancies.forEach((e,i) => {
+  competenciesDiv.innerHTML += `
+   <div class="testimonial">
+    <i class="${e.logo}"></i>
+    <div class="text">
+     <p>${e.text}</p>
+    </div>
+    <div class="author-info">
+     <h4 class="author">${e.competencyName}</h4>
+     <div class="icon">
+      <i class="${e.competencyFontAwesomeClassName}"></i>
+     </div>
+    </div>
+   </div>
+  `;
+ });
+ //Conditional based off config object property tsscutsPricingState. Which can be on/off.
+ if (obj.tsscutsPricingState == "on") {
+  pricingDivState.style.display = "block";
+  let pricingDiv = document.getElementById('pricing');
+  obj.tsscutsPricing.forEach((e,i) => {
+   function generateList (listItemsArray) {
+    let html = '';
+    for (let i = 0; i < listItemsArray.length; i++) {
+     html += `<div class="fw-default-row">${listItemsArray[i]}</div>`
+    }
+    return html;
+   }
+   pricingDiv.innerHTML += `
+    <div class="fw-package-wrap col-md">
+     <div class="fw-package">
+      <div class="fw-heading-row">
+       <span>${e.pricingLevel}</span>
+      </div>
+      <div class="fw-pricing-row">
+       <span>$${e.pricingRate}</span>
+       <small>per hour</small>
+      </div>
+      ${generateList(e.pricingList)}
+     </div>
+    </div>
+   `;
+  });
+ }
+ //Fill in Knowledges Area
+ knowledgesDiv.innerHTML = "";
+ obj.tsscutsKnowledges.forEach((e,i) => {
+  knowledgesDiv.innerHTML += `<li>${e}</li>`;
+ });
+ //Fill in Skills Area
+ skillsDiv.innerHTML = "";
+ obj.tsscutsSkills.forEach((e,i) => {
+  if (e.skillTitle) {
+   skillsDiv.innerHTML += `
+    <div class="block-title"><h3> ${e.skillTitle} <span>Skills</span></h3></div>
+    <div class="skills-info skills-second-style" id="${e.skillTitle.toLowerCase()}-${i}"></div>
+    <div class="white-space-10"></div>
+   `;
+   let innerSkillsDiv = document.getElementById(`${e.skillTitle.toLowerCase()}-${i}`);
+   for (let pi = 0; pi < e.profeciencies.length; pi++) {
+    innerSkillsDiv.innerHTML += `
+     <div class="skill clearfix">
+      <h4>${e.profeciencies[pi].name}</h4>
+      <div class="skill-value">${e.profeciencies[pi].percentage}%</div>
+     </div>
+     <div class="skill-container skill-${pi}">
+      <div class="skill-percentage" style="width:${e.profeciencies[pi].percentage}%"></div>
+     </div>
+    `;
+    if (e.profeciencies.length-1 == pi) innerSkillsDiv.innerHTML += `</div><div class="white-space-10"></div>`;
+   }
+  }
+ });
+ //Fill in Certificates Area
+ certificatesDiv.innerHTML = "";
+ obj.tsscutsCertificates.forEach((e,i) => {
+  certificatesDiv.innerHTML += `
+   <div class="col-xs-12 col-sm-6">
+    <div class="certificate-item clearfix">
+     <div class="certi-logo">
+      <img src="images/${e.logo}" alt="${e.vendor}-logo" onerror="this.parentElement.innerHTML='${e.vendor}'">
+     </div>
+     <div class="certi-content">
+      <div class="certi-title">
+       <h4>${e.title}</h4>
+       <h6 style="font-weight: 400;font-style: italic;">${e.vendor}</h6>
+      </div>
+      <div class="certi-id">
+       <span>ID: ${e.id}</span>
+      </div>
+      <div class="certi-date">
+       <span>${e.date}</span>
+      </div>
+      <div class="certi-company">
+       <span></span>
+      </div>
+     </div>
+    </div>
+   </div>
+  `;
+ });
+ //Fill in Education Area
+ educationDiv.innerHTML = "";
+ obj.tsscutsEducation.forEach((e,i) => {
+  educationDiv.innerHTML += `
+   <div class="timeline-item clearfix">
+    <div class="left-part">
+     <h5 class="item-period">${e.range}</h5>
+     <span class="item-company" style="font-weight: 600;">${e.gpa}</span>
+     <span class="item-company">${e.schoolName}</span>
+    </div>
+    <div class="divider"></div>
+    <div class="right-part">
+     <h4 class="item-title">${e.fieldOfStudy}</h4>
+     <p>${e.description}</p>
+    </div>
+   </div>
+  `;
+ });
+ //Fill in Experience Area
+ experienceDiv.innerHTML = "";
+ obj.tsscutsExperience.forEach((e,i) => {
+  experienceDiv.innerHTML += `
+   <div class="timeline-item clearfix">
+    <div class="left-part">
+     <h5 class="item-period">${e.range}</h5>
+     <span class="item-company">${e.companyName}</span>
+    </div>
+    <div class="divider"></div>
+    <div class="right-part">
+     <h4 class="item-title">${e.title}</h4>
+     <p>${e.description}</p>
+    </div>
+   </div>
+  `;
+ });
+}
+
+//Take an array of image file names and the id of the img element
+function randomPicture(pa) {
+ let selectedObj = pa[Math.floor(Math.random() * pa.length)];
+ let link = document.getElementById('artistLink');
+ let backgroundImage = document.getElementById('particles-js');
+ backgroundImage.style.backgroundImage = `url('../public/images/backgrounds/${selectedObj.image}')`;
+ link.href = `${selectedObj.link}`;
+ link.innerHTML = `Background Image: ${selectedObj.author}`
+}
+
+function randomPageColor(ca) {
+ let selectedObj = ca[Math.floor(Math.random() * ca.length)];
+ document.documentElement.setAttribute("style", `
+  --tsscuts-color-primary-1: ${selectedObj.tsscutsColorPrimaryOne};
+  --tsscuts-color-primary-2: ${selectedObj.tsscutsColorPrimaryTwo};
+  --tsscuts-color-secondary: ${selectedObj.tsscutsColorSecondary};
+  --tsscuts-color-secondary-header: ${selectedObj.tsscutsColorSecondaryHeader};
+ `);
+}
+
+function defaultErrorHandler (error) {
+ let returnObj = {"status": "","message": "","payload": ""};
+ //This is done just incase you use the "throw" keyword to produce your own error.
+ let errorMessage = ((error.message) ? error.message:error);
+ returnObj.status = "failure";
+ returnObj.message = `Function: ${arguments.callee.caller.name} - Error: ${errorMessage}`;
+ returnObj.payload = error;
+ console.error(returnObj);
+ return returnObj;
+}
+
+//The following uses Sweet Alerts 2 library which needs to be called before hand.
+function popupErrorHandler (error,targetDiv) {
+ let errorMessage = ((error.message) ? error.message:error);
+ try {
+/*
+  Swal.fire({
+   icon: 'error',
+   title: 'Error: Check Console',
+   target: (targetDiv ? `#${targetDiv}`:'body'),
+   allowEscapeKey: true,
+   allowOutsideClick: true,
+   showConfirmButton: false,
+   customClass: {
+    container: 'position-absolute'
+   },
+   html: `${errorMessage}<br><br><button type="button" class="btn btn-icon btn-rounded btn-outline-danger" style="width: auto;height: 30px;padding: 3px 10px;cursor: pointer;" onclick="Swal.close();"><i class="feather icon-x-circle"></i>&nbsp;Close</button>`,
+  });
+*/
+  return error;
+ } catch (e) {
+  return defaultErrorHandler(e);
+ } finally {}
+}
+
+var pictureArray = [
+ {"image":"martin-mart-3d-grass-collection-vol01-02.jpg","link":"https://www.artstation.com/artwork/rA0AoO","author":"Superb CG"},
+ {"image":"pawel-pecherzewski-entrance-shadow-final-01.jpg","link":"https://www.artstation.com/artwork/0v0bV","author":"Paweł Pęcherzewski"},
+ {"image":"pawel-pecherzewski-blue-hour-foggy-post.jpg","link":"https://www.artstation.com/artwork/OodkD6","author":"Paweł Pęcherzewski"},
+ {"image":"dasom-hyun-20-2.jpg","link":"https://www.artstation.com/artwork/18YER2","author":"Dasom Hyun"},
+ {"image":"milan-vasek-forest-test.jpg","link":"https://www.artstation.com/artwork/L34bbv","author":"Milan Vasek"},
+ {"image":"lap-pun-cheung-the-grasslands-the-old-bridge.jpg","link":"https://www.artstation.com/artwork/2B8oA","author":"Lap Pun Cheung"},
+ {"image":"joe-garth-bliss-v1.jpg","link":"https://www.artstation.com/artwork/zOVVPQ","author":"Joe Garth"}
+];
+
+var colorArray = [
+ {
+  "tsscutsColorPrimaryOne":"rgba(11,163,118,0.8)",
+  "tsscutsColorPrimaryTwo":"rgba(11,112,128,0.8)",
+  "tsscutsColorSecondary":"rgba(11,163,118,0.8)",
+  "tsscutsColorSecondaryHeader":"rgba(11,163,118)"
+ },
+];
+
+async function engineControl (controlWord) {
+ let returnObj = {"status": "","message": "","payload": ""};
+ try {
+  if (controlWord == 'start') console.log('Engine Start');
+  if (controlWord == 'stop') console.log('Engine Stop');
+  returnObj.status = "success";
+  returnObj.message = "success message";
+  returnObj.payload = "";
+  return returnObj;
+ } catch (e) {
+  return defaultErrorHandler(e);
+ } finally {}
+}
+
+randomPicture(pictureArray);
+randomPageColor(colorArray);
+// fillInterface(tsscutsConfig);
+document.addEventListener('readystatechange', event => {
+ if (event.target.readyState === 'complete') {
+  let targetOwlDiv = document.getElementById('competencies').querySelector('.owl-stage-outer.owl-height');
+  targetOwlDiv.style.height = 'auto';
+ }
+});
+
+//Radial range
+//Source: https://codepen.io/MyXoToD/pen/xGRrgQ 
+/*
+(function() {
+  $(document).ready(function() {
+    var is_dragging;
+    is_dragging = false;
+    $(document).on("mousedown touchstart", ".circle", function(e) {
+      return is_dragging = true;
+    });
+    $(document).on("mouseup touchend", function(e) {
+      return is_dragging = false;
+    });
+    return $(window).on("mousemove touchmove", function(e) {
+      var angle, center_x, center_y, circle, delta_x, delta_y, pos_x, pos_y, touch;
+      if (is_dragging) {
+        circle = $(".circle");
+        touch = void 0;
+        if (e.originalEvent.touches) {
+          touch = e.originalEvent.touches[0];
+        }
+        center_x = ($(circle).outerWidth() / 2) + $(circle).offset().left;
+        center_y = ($(circle).outerHeight() / 2) + $(circle).offset().top;
+        pos_x = e.pageX || touch.pageX;
+        pos_y = e.pageY || touch.pageY;
+        delta_y = center_y - pos_y;
+        delta_x = center_x - pos_x;
+        angle = Math.atan2(delta_y, delta_x) * (180 / Math.PI); // Calculate Angle between circle center and mouse pos
+        angle -= 90;
+        if (angle < 0) {
+          angle = 360 + angle; // Always show angle positive
+        }
+        angle = Math.round(angle);
+        $(".dot").css("transform", "rotate(" + angle + "deg)");
+        return $(".debug").html(angle + "deg");
+      }
+    });
+  });
+}).call(this);
+*/
