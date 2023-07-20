b71e047f08c1e45b2288633215e9571036569eb4
Tree Star Systems <info@treestarsystems.com>
2023-07-19 23:57:19 -0400

    update

--- server/core/fanRegulator.js
+++ server/core/fanRegulator.js
@@ -1,4 +1,5 @@
 /*
+ Documentation: https://www.raspberrypi.com/documentation/computers/config_txt.html#monitoring-core-temperature
  Purpose - A collection of fan control functions
 */
 const EventEmitter = require('events');
@@ -44,17 +45,17 @@ async function fanRegulator (fro) {
     s = speed (aka brightness in Johnny-Five)
    */
    adjustFan.on('changeFanSpeed', (s) => {
-//    console.log(`Speed: ${s} | Temp: ${temp}${defaultScale.toUpperCase()}`);
+    console.log(`Speed: ${s} | Temp: ${temp}${defaultScale.toUpperCase()}`);
     fans.brightness(s);
    });
 
    adjustFan.on('off', () => {
-//    console.log(`Fan Off: Speed: 0 | Temp: ${temp}${defaultScale.toUpperCase()}`);
+    console.log(`Fan Off: Speed: 0 | Temp: ${temp}${defaultScale.toUpperCase()}`);
     fans.off();
    });
 
    adjustFan.on('emergencyShutdown', (s) => {
-//    console.log(`Shutting Down: Speed: ${s} | Temp: ${temp}${defaultScale.toUpperCase()}`);
+    console.log(`Shutting Down: Speed: ${s} | Temp: ${temp}${defaultScale.toUpperCase()}`);
     clearInterval(calculateAverageTemp);
    })
   });
@@ -75,17 +76,17 @@ async function fanRegulator (fro) {
     }
    }
    //Emit an event that is passed an array of pins the fans are connected to and the speed
-//temp = core.getRandomInt(0,109)
-   //Turn off below 80F
-   if (temp < 80) adjustFan.emit('off');
-   //Set fan speed to Low between 80+ to 90F
-   if (temp > 80 && temp <= 90) adjustFan.emit('changeFanSpeed', 170);
-   //Set fan speed to Medium between 90+ to 100F
-   if (temp > 90 && temp <= 100) adjustFan.emit('changeFanSpeed', 213);
-   //Set fan speed to High between 100+ to 110F
-   if (temp > 100 && temp <= 110) adjustFan.emit('changeFanSpeed', 255);
-   //Shut system when greater than 110F
-   if (temp > 110) adjustFan.emit('emergencyShutdown', 0);
+//temp = core.getRandomInt(0,190)
+   //Turn off below 150F
+   if (temp < 150) adjustFan.emit('off');
+   //Set fan speed to Low between 150+ to 160F
+   if (temp > 150 && temp <= 160) adjustFan.emit('changeFanSpeed', 170);
+   //Set fan speed to Medium between 160+ to 170F
+   if (temp > 160 && temp <= 170) adjustFan.emit('changeFanSpeed', 213);
+   //Set fan speed to High between 170+ to 180F
+   if (temp > 170 && temp <= 180) adjustFan.emit('changeFanSpeed', 255);
+   //Shut system when greater than 180F
+   if (temp > 180) adjustFan.emit('emergencyShutdown', 0);
   }
 
   let calculateAverageTemp = setInterval (() => {
